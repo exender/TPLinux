a5ab0e1863dc98b0bf8380261e1104b598a3638f
Baptiste Bonnin <baptiste.bonnin@ynov.com>
2020-09-24 02:54:35 +0200

    add du script de sauvegarde

---
+++ sauvegarde.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# date du jour
+backupdate=$(date +%Y-%m-%d)
+
+#répertoire de backup
+dirbackup=/backup/backup-$backupdate
+
+# création du répertoire de backup
+/bin/mkdir $dirbackup
+
+# tar -cjf /destination/fichier.tar.bz2 /source1 /source2 /sourceN
+# créé une archive bz2
+# sauvegarde de /home
+/bin/tar -cjf $dirbackup/home-$backupdate.tar.bz2 /home
+
+# sauvegarde mysql
+/usr/bin/mysqldump --user=xxxx --password=xxxx --all-databases | /usr/bin/gzip > $dirbackup/mysqldump-$backupdate.sql.gz
